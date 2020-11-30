2e52caea4f9aeae36c4cbbeb434e0ba4e5b8b34e
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-30 18:43:29 +0200

    Add user registration endpoint

--- inc/API.class.php
+++ inc/API.class.php
@@ -15,10 +15,62 @@ class API
     {
         switch( $action )
         {
+            case 'register':
+                self::handleRegistration();
+                break;
+
             default:
                 HTTP::_404();
                 break;
         }
+    }
+
+
+    // handle new user registration
+    private static function handleRegistration()
+    {
+        /**
+         * Validations
+         */
+        $requiredFields = [
+            'user-name',
+            'password',
+            'password-confirm'
+        ];
+
+        if( sizeof( checkRequired( $_POST, $requiredFields ) ) != 0 )
+        {
+            $error = self::createError( 'Please fill all required fields' );
+            HTTP::_400( $error );
+        }
+
+        if( $_POST['password'] != $_POST['password-confirm'] )
+        {
+            $error = self::createError( 'Passwords do not match' );
+            HTTP::_400( $error );
+        }
+
+
+        /**
+         * Create user
+         */
+        $user = DB::getInstance()->insert([
+
+            'userName'  => $_POST['user-name'],
+            'password'  => password_hash( $_POST['password'], PASSWORD_DEFAULT ),
+            'isOnline'  => true,
+            'userAgent' => $_SERVER['HTTP_USER_AGENT'],
+            'ip'        => $_SERVER['REMOTE_ADDR']
+
+        ]);
+
+
+        // prevent sending the password in the response
+        unset( $user->password );
+
+        $message = self::createMessage( 'Created successfully', $user );
+        HTTP::_201( $message );
+
     }
 
 
