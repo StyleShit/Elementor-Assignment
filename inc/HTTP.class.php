82f518b8f25c9663dbaf5210670e0cb4e978a5ee
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-30 18:29:27 +0200

    Create HTTP responses class

---
+++ inc/HTTP.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * handle HTTP code responses
+ */
+class HTTP
+{
+    private static function json()
+    {
+        header('Content-Type: application/json');
+    }
+
+    
+    public static function _200( $message = [] )
+    {
+        self::json();
+        header( 'HTTP/1.0 200 OK' );
+        die( json_encode( $message ) );
+    }
+
+
+    public static function _201( $message = [] )
+    {
+        self::json();
+        header( 'HTTP/1.0 201 Created' );
+        die( json_encode( $message ) );
+    }
+
+
+    public static function _404( $message = [] )
+    {
+        self::json();
+        header( 'HTTP/1.0 404 Not Found' );
+        die( json_encode( $message ) );
+    }
+
+
+    public static function _400( $message = [] )
+    {
+        self::json();
+        header( 'HTTP/1.0 400 Bad Request' );
+        die( json_encode( $message ) );
+    }
+}
