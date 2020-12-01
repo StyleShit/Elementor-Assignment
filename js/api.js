fce807e3d667b501fefa6f5edb58a7a49304bca1
Evyatar Daud <evyatar.daud@gmail.com>
2020-12-01 10:09:56 +0200

    Create API requests functions

---
+++ js/api.js
@@ -0,0 +1,140 @@
+/**
+ * Handle API requests
+ */
+
+
+const API_URL = 'api.php';
+
+
+// make request
+const _apiRequest = ({ method = 'GET', data = {}, action = '' }) => {
+
+    let url = `${ API_URL }?action=${ action }`;
+    let options = { 
+
+		method, 
+		mode: 'cors', 
+		redirect: 'follow'
+
+	};
+
+	// if it's GET, add 'data' as query string
+	if( method === 'GET' )
+	{
+		const queryString = Object.keys( data ).map( key => ( key + '=' + data[key] )).join( '&' );
+		url += queryString ? '&' + queryString : '';
+	}
+
+	// other methods, add 'data' as body
+	else
+	{
+		options.body = _jsonToFormData( data );
+	}
+
+
+    return fetch( url, options ).then( res => res.json() );
+
+};
+
+
+// register user
+const _apiRegisterUser = ({ email, password, passwordConfirm }) => {
+
+	return _apiRequest({
+		method: 'POST',
+		action: 'register',
+		data: {
+			email,
+			password,
+			'password-confirm': passwordConfirm
+		}
+	});
+
+}
+
+
+// login user
+const _apiLoginUser = ({ email, password }) => {
+
+	return _apiRequest({
+		method: 'POST',
+		action: 'login',
+		data: {
+			email,
+			password
+		}
+	});
+
+}
+
+
+// logout user
+const _apiLogoutUser = () => {
+
+	return _apiRequest({
+		action: 'logout'
+	});
+
+}
+
+
+// set user in online state
+const _apiGoOnline = () => {
+
+	return _apiRequest({
+		action: 'go-online'
+	});
+
+}
+
+
+// set user in offline state
+const _apiGoOffline = () => {
+
+	return _apiRequest({
+		action: 'go-offline'
+	});
+
+}
+
+
+// get online users
+const _apiGetOnlineUsers = () => {
+
+	return _apiRequest({
+		action: 'get-online-users'
+	});
+
+}
+
+
+// get user data by id
+const _apiGetUserData = ({ id }) => {
+
+	return _apiRequest({
+		action: 'get-user',
+		data: {
+			'user-id': id
+		}
+	});
+
+}
+
+
+/**
+ * Helpers
+ */
+
+// convert JSON object to FormData
+const _jsonToFormData = ( json ) => {
+
+	let formData = new FormData();
+
+	for( var key in json )
+	{
+		formData.append( key, json[key] );
+	}
+
+	return formData;
+
+};
